40410de79e32b3a7aea61c8ec9a6a43ed27ea3e8
thaduricoder <95437771+thaduricoder@users.noreply.github.com>
2021-12-11 20:33:00 +0530

    DateAndTIme.java

---
+++ DateAndTIme.java
@@ -0,0 +1,9 @@
+// To display present date and time
+import java.util.Date;
+
+public class DateAndTIme {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println(now);
+    }
+}

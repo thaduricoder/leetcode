a82182c07922cff2c25b484021c9d10bb3c51aa2
thaduricoder <95437771+thaduricoder@users.noreply.github.com>
2021-12-13 19:33:57 +0530

    Delete DateAndTIme.java

--- DateAndTIme.java
+++
@@ -1,9 +0,0 @@
-// To display present date and time
-import java.util.Date;
-
-public class DateAndTIme {
-    public static void main(String[] args) {
-        Date now = new Date();
-        System.out.println(now);
-    }
-}
